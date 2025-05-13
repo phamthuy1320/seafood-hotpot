87108b78a6f3b97ef012f6dfc60aa88a4f7e1f98
thuypt <phamthithuy1320@gmail.com>
2025-05-13 21:45:59 +0700

    Update toggle - Props type

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -4,12 +4,12 @@ type ToggleProps<T = string> = {
   onChange?: (value: T) => void;
 };
 
-export function Toggle<T>({ options = [], value, onChange }: ToggleProps) {
+export function Toggle<T>({ options = [], value, onChange }: ToggleProps<T>) {
   return (
     <div>
       {options.map(({ value, label }, index) => (
         <label key={index}>
-          <input type="checkbox" value={value} hidden />
+          <input type="checkbox" value={String(value)} hidden />
           {label}
         </label>
       ))}
